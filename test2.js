a5799e86a884e74a82c89865f20b3bfe53ec703e
tangli <tangli@mobimagic.com>
2021-09-08 20:32:01 +0800

    商户餐

--- test2.js
+++ test2.js
@@ -2,6 +2,6 @@
 window.onload=function(){
     let box=document.getElementsByClassName('box')[0]
      box.addEventListener('click',()=>{
-         alert('这是测试代码')
+         alert('这是测试代码yayay')
      })
  }
